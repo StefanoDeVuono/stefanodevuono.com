882e7ee5518b735810ff0465c488156a3527ad42
Stefano DeVuono <fon@jackfresno.ca>
2013-03-31 00:28:22 -0600

    Improved UI for Desktop and Mobile
    - nav menu works in Desktop and Mobile
    - Adjustments to where stuff scrolls to
    - Header stays fixed in mobile

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
-var menu = false;
+var menu = false; // start w/o menu
+yPos = 0; // start at y position 0
 
-function showHide() {
-	if (menu == false) {
+// menu for mobile
+$('#touch').on('touchstart', function(e){
+	//alert('show');
+	e.preventDefault();
+	if ( menu == false ) {
 		$('nav a').show();
+		yPos = $('#page').scrollTop();
+		console.log('show menu ' + yPos);
 		$('section').hide();
 		menu = true;
-	} else if (menu == true) {
+	} else {
+		console.log('else');
 		$('nav a').hide();
-		$('section').show();
-		menu = false;
-	} 
-}
-
-$('#touch').on('touchstart', showHide);
-
-
-$('nav').on('touchstart click', 'a', function(e){
-	e.preventDefault();
-
-	if ( $('#touch').is(':visible') ) showHide();
-
-	var destination = $(this).attr('href');
-
-	console.log(destination);
-
-	$('html, body').animate({
-		scrollTop: $(destination).offset().top
-	}, 1000,'swing');
-
+ 		$('section').show();
+ 		$('#page').scrollTop(yPos);
+ 		menu = false;
+	}
 });
+
+
+$('nav a').on('touchstart click', function(e){
+ 	e.preventDefault();
+ 	var destination = $(this).attr('href'); // anchor to part of page we're going to
+ 	if ( menu == true && $('#touch').is(':visible') ) {
+ 		$('nav a').hide();
+ 		$('section').show();
+ 		$('#page').scrollTop(yPos);
+ 		menu = false;
+ 		$('#page').animate({ // animated scroll for mobile
+			scrollTop: $('#page').scrollTop() + $(destination).offset().top
+		}, 1000,'swing');
+ 	}
+ 	if ( $('#touch').is(':hidden') ) {
+ 		console.log('hello');
+ 		$('html, body').animate({ // animated scroll for desktop
+			scrollTop: $(destination).offset().top
+		}, 1000,'swing');
+ 	}
+	
+ 	
+ });
