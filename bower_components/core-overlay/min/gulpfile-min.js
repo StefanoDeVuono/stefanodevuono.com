3c9960a4aad6d9c38b6947586909cc1d38c55f96
Stefano De Vuono <io@stefanodevuono.com>
2015-05-07 14:11:07 -0700

    Newer tings

---
+++ bower_components/core-overlay/min/gulpfile-min.js
@@ -0,0 +1,1 @@
+var gulp=require("gulp");require("gulp-web-component-tester").init(gulp);
---
+++ bower_components/web-animations-js/templates/min/web-animations-min.js
@@ -0,0 +1,1 @@
+var webAnimationsSourceTarget="<%= target %>",WEB_ANIMATIONS_TESTING=!1;!function(){var t=document.getElementsByTagName("script"),e=t[t.length-1].src.replace(/[^\/]+$/,"");document.write('<script src="'+e+'target-config.js"></script>'),document.write('<script src="'+e+'target-loader.js"></script>')}();
